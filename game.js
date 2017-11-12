6e5adf05febc01f6269c11110b3d92091140781c
Alex Ruiz <ruiz.alex0302@gmail.com>
2017-11-11 23:52:57 -0800

    Anti-cheating

--- game.js
+++ game.js
@@ -20,3 +20,7 @@ $(function () {
     puns.innerHTML = puns.innerHTML + "<li> <p>They said: </p>" + msg + "</li>"
   });
 });
+
+function pasteNotice(){
+  alert("Please don't paste pick up lines! Naughty!");
+}
